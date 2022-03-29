add253fd3923ae059efa959bb7c9b1aaff2faf86
Nando N <ichbinando@gmail.com>
2022-03-29 09:48:55 -0300

    Requisitos 5, 6, 7 e 8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,34 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import validator from 'validator';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import UserContext from '../context/userContext';
 
 const Login = () => {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const history = useHistory();
   const { setLoggedUser } = useContext(UserContext);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const MIN = 6;
+
+  // Fazer validação do email e senha.
+  // Usar useEffect passando como 2ndo arg. um arr com as dependências email e password,
+  // para rodar a validação toda vez que esses estados mudarem.
+  useEffect(() => {
+    const flag = validator.isEmail(email) && password.length > MIN;
+    console.log(flag);
+    if (flag) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [email, password]);
 
   // Controlar submit do form, prevenindo comport. padrão,
   // chamar setLoggedUser pra setar o email do Provider,
   // e redirecionar para pag. Foods.
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoggedUser(email);
@@ -36,14 +52,15 @@ const Login = () => {
           type="password"
           data-testid="password-input"
           name="Password"
-          placeholder="*********"
-          onChange={ () => {} }
+          placeholder="******"
+          onChange={ (e) => setPassword(e.target.value) }
         />
       </div>
       <Button
         type="submit"
         data-testid="login-submit-btn"
         value="Enter"
+        disabled={ isDisabled }
       />
     </form>
   );
