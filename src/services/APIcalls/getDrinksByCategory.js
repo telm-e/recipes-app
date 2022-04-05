334f58fa75f949d7e1f4e1552090b30551424dbb
Telme <telmaeugenio@gmail.com>
2022-04-05 11:55:49 -0300

    fix: API url misspelling

--- src/services/APIcalls/getDrinksByCategory.js
+++ src/services/APIcalls/getDrinksByCategory.js
@@ -1,6 +1,6 @@
 const getDrinksByCategory = async (cat) => {
   try {
-    const url = `www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`;
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
