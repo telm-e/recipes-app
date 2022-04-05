119d5572a943e4b54ea02acdda05d2736e4b24af
Telme <telmaeugenio@gmail.com>
2022-04-05 08:54:48 -0300

    feat: categories button - spec 27

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -6,6 +6,7 @@ import Loading from '../components/Loading';
 import Header from '../components/Header';
 import getDrinksByName from '../services/APIcalls/getDrinksByName';
 import DrinkCard from '../components/DrinkCard';
+import getDrinkCategories from '../services/APIcalls/getDrinkCategories';
 
 function Drinks() {
   const {
@@ -15,20 +16,28 @@ function Drinks() {
     isSearchDisabled,
   } = useContext(recepiesContext);
   const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
   const notFoundAlert = 'Sorry, we haven\'t found any recipes for these filters.';
 
-  useEffect(async () => {
-    const recepiesData = await getDrinksByName('');
-    const arrLength = recepiesData.drinks.length;
-    const MAX_ARR_LENGTH = 12;
-    if (arrLength > MAX_ARR_LENGTH) {
-      const newArr = recepiesData.drinks.splice(0, MAX_ARR_LENGTH);
-      console.log(newArr);
-      setData(newArr);
-    } else {
-      const newArr = recepiesData.drinks;
-      setData(newArr);
-    }
+  useEffect(() => {
+    (async () => {
+      const CATEGORIES_NUMBER = 5;
+      const recepiesCategories = await getDrinkCategories();
+      const cat = recepiesCategories.drinks.splice(0, CATEGORIES_NUMBER);
+      setCategories(cat);
+      console.log(cat);
+      const recepiesData = await getDrinksByName('');
+      const arrLength = recepiesData.drinks.length;
+      const MAX_ARR_LENGTH = 12;
+      if (arrLength > MAX_ARR_LENGTH) {
+        const newArr = recepiesData.drinks.splice(0, MAX_ARR_LENGTH);
+        console.log(newArr);
+        setData(newArr);
+      } else {
+        const newArr = recepiesData.drinks;
+        setData(newArr);
+      }
+    })();
   }, []);
 
   return (
@@ -38,6 +47,15 @@ function Drinks() {
       </div>
       { isSearchDisabled
           && <DrinkSearchHeader />}
+      { categories.map((each, index) => (
+        <button
+          key={ index }
+          type="button"
+          data-testid={ `${each.strCategory}-category-filter` }
+        >
+          { each.strCategory }
+        </button>
+      ))}
       { searchStatus ? (
         <div>
           { loading ? <Loading /> : (
