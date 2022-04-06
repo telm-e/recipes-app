9fac498bf01a4ced78aa41b2263ece9fca7a609f
Telme <telmaeugenio@gmail.com>
2022-04-06 17:50:48 -0300

    feat: in-progress pages & checkbox ingredients - spec 47, 48, 49

--- src/pages/InProgressDrinks.jsx
+++ src/pages/InProgressDrinks.jsx
@@ -1,19 +1,148 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Proptypes from 'prop-types';
+import ButtonShare from '../components/ButtonShare';
+import ButtonFavorite from '../components/ButtonFavorite';
 
-const InProgressDrinks = (props) => {
+function InProgressDrinks(props) {
+  const [getDrinks, setDrinks] = useState();
+  const [getMeals, setGetMeals] = useState();
+
   const { match: { params: { id } } } = props;
-  return (
-    <div>
-      InProgressDrinks;
-      { id }
-    </div>
+  const history = useHistory();
 
+  useEffect(() => {
+    const getApiDrink = async () => {
+      const { drinks } = await (
+        await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)).json();
+      setDrinks(drinks);
+    };
+
+    const getApiMeal = async () => {
+      const { meals } = await (await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')).json();
+      setGetMeals(meals);
+    };
+
+    getApiDrink();
+    getApiMeal();
+  }, [id]);
+
+  if (!getDrinks || !getMeals) return <h1>Loading...</h1>;
+
+  const {
+    strDrink,
+    strAlcoholic,
+    strInstructions,
+    strDrinkThumb,
+  } = getDrinks[0];
+
+  const allIngredients = Object.entries(getDrinks[0])
+    .filter((item) => item[0].includes('strIngredient'));
+
+  const ingredients = allIngredients
+    .filter((item) => item[1] ?? item[1])
+    .map((item) => item[1]);
+
+  const allMeasures = Object.entries(getDrinks[0])
+    .filter((item) => item[0].includes('strMeasure'));
+
+  const measures = allMeasures
+    .filter((item) => ((item[1]) && item[1] !== ' ' ? item[1] : false))
+    .map((item) => item[1]);
+
+  const recipe = {
+    id,
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: strAlcoholic,
+    name: strDrink,
+    image: strDrinkThumb,
+  };
+  // Desenvolva um botão de nome "Start Recipe" que deve ficar fixo na parte de baixo da tela o tempo todo
+  const style = {
+    position: 'fixed',
+    bottom: '0',
+  };
+
+  return (
+    <section>
+      <div>
+        <img
+          data-testid="recipe-photo"
+          src={ strDrinkThumb }
+          alt={ strDrink }
+        />
+      </div>
+
+      <div>
+        <div>
+          <h1 data-testid="recipe-title">{ strDrink }</h1>
+          <ButtonShare path={ history.location.pathname } />
+          <ButtonFavorite recipe={ recipe } />
+        </div>
+
+        <h3 data-testid="recipe-category" style={ { color: 'gray' } }>
+          { strAlcoholic }
+        </h3>
+
+        <div>
+          <h3>Ingredients</h3>
+          <div
+            style={ { backgroundColor: '#DCDCDC',
+              borderRadius: '4px',
+              padding: '1% 2%' } }
+          >
+            {
+              ingredients.map((item, index) => (
+                <li
+                  key={ index }
+                  data-testid={ `${index}-ingredient-step` }
+                  style={ { listStyle: 'none' } }
+                >
+                  <input
+                    id={ index }
+                    type="checkbox"
+                  />
+                  { `- ${item} - ${measures[index]}` }
+                </li>))
+            }
+          </div>
+        </div>
+
+        <div>
+          <h3>Instructions</h3>
+          <p
+            style={ { backgroundColor: '#DCDCDC',
+              borderRadius: '4px',
+              padding: '1% 2%' } }
+            data-testid="instructions"
+          >
+            { strInstructions }
+          </p>
+        </div>
+
+        <Link
+          style={ style }
+          className="finish-button"
+          to="/done-recipes"
+          data-testid="finish-recipe-btn"
+        >
+          <button
+            type="button"
+          >
+            Finish recipe
+          </button>
+        </Link>
+      </div>
+
+    </section>
   );
-};
+}
 
 InProgressDrinks.propTypes = {
-  id: PropTypes.string,
-  props: PropTypes.string,
+  path: Proptypes.string,
+  props: Proptypes.string,
 }.isRequired;
+
 export default InProgressDrinks;
--- src/pages/InProgressFoods.jsx
+++ src/pages/InProgressFoods.jsx
@@ -1,56 +1,130 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Proptypes from 'prop-types';
+import ButtonShare from '../components/ButtonShare';
+import ButtonFavorite from '../components/ButtonFavorite';
 
 function InProgressFoods(props) {
+  const [getMeals, setGetMeals] = useState([]);
+  const [getDrinks, setGetDrinks] = useState([]);
+
   const { match: { params: { id } } } = props;
-  // Desenvolva um botão de nome "Start Recipe" que deve ficar fixo na parte de baixo da tela o tempo todo
+  const history = useHistory();
+
+  // Verifica se a requisição para a API de comidas foi realizada
+  useEffect(() => {
+    const getApiMeal = async () => {
+      const { meals } = await (await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)).json();
+      setGetMeals(meals);
+    };
+    const getApiDrink = async () => {
+      const { drinks } = await (await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')).json();
+      setGetDrinks(drinks);
+    };
+    getApiMeal();
+    getApiDrink();
+  }, [id]);
+
+  if (getMeals.length === 0 || getDrinks.length === 0) return <h1>Loading...</h1>;
+
+  const {
+    strMeal,
+    strCategory,
+    strInstructions,
+    strMealThumb,
+    strArea,
+  } = getMeals[0];
+  // Retorna um array do objeto
+  const allIngredients = Object.entries(getMeals[0])
+    .filter((item) => item[0].includes('strIngredient'));
+  const ingredients = allIngredients
+    .filter((item) => item[1] ?? item[1])
+    .map((item) => item[1]);
+  const allMeasures = Object.entries(getMeals[0])
+    .filter((item) => item[0].includes('strMeasure'));
+  const measures = allMeasures
+    .filter((item) => ((item[1]) && item[1] !== ' ' ? item[1] : false))
+    .map((item) => item[1]);
+
+  const recipe = {
+    id,
+    type: 'food',
+    nationality: strArea,
+    category: strCategory,
+    alcoholicOrNot: '',
+    name: strMeal,
+    image: strMealThumb,
+  };
+    // Desenvolva um botão de nome "Finish Recipe" que deve ficar fixo na parte de baixo da tela o tempo todo
   const style = {
     position: 'fixed',
     bottom: '0',
   };
+
   return (
-    <div>
+    <section>
+      <div>
+        <img
+          data-testid="recipe-photo"
+          src={ strMealThumb }
+          alt={ strMeal }
+        />
+      </div>
+      <div>
+        <div>
+          <h1 data-testid="recipe-title">{ strMeal }</h1>
+          <ButtonShare path={ history.location.pathname } />
+          <ButtonFavorite recipe={ recipe } />
+        </div>
+        <h3 data-testid="recipe-category" style={ { color: 'gray' } }>{ strCategory }</h3>
+        <div>
+          <h3>Ingredients</h3>
+          <div>
+            {
+              ingredients.map((item, index) => (
+                <li
+                  key={ index }
+                  data-testid={ `${index}-ingredient-step` }
+                  style={ { listStyle: 'none' } }
+                >
+                  <input
+                    id={ index }
+                    type="checkbox"
+                  />
+                  { `- ${item} - ${measures[index]}` }
+                </li>))
+            }
+          </div>
+        </div>
+        <div>
+          <h3>Instructions</h3>
+          <p
+            data-testid="instructions"
+          >
+            { strInstructions }
+          </p>
+        </div>
 
-      <h1
-        data-testid="recipe-title"
-      >
-        Foods Details
-      </h1>
-      <img
-        src="recipePhoto"
-        alt="recipefoods"
-        data-testid="recipe-photo"
-      />
-      <section>
-        <h2>Category</h2>
-        <p data-testid="recipe-category">category</p>
-        <h2>Ingredients</h2>
-        <ul data-testid="0-ingredient-name-and-measure">
-          <li>Ingredients</li>
-        </ul>
-        <h2>Instructions</h2>
-        <p data-testid="instructions">instructions</p>
-      </section>
-      <button type="button" data-testid="share-btn">Share</button>
-      <label htmlFor="favorite-btn">
-        <input type="checkbox" data-testid="favorite-btn" id="favorite-btn" />
-        Favorite
-      </label>
-      <button
-        style={ style }
-        type="button"
-        data-testid="finish-recipe-btn"
-      >
-        Finish Recipe
-      </button>
-      { id }
-    </div>
-
+        <Link
+          style={ style }
+          className="finish-button"
+          to="/done-recipes"
+          data-testid="finish-recipe-btn"
+        >
+          <button
+            type="button"
+          >
+            Finish recipe
+          </button>
+        </Link>
+      </div>
+    </section>
   );
 }
 
 InProgressFoods.propTypes = {
-  id: PropTypes.string,
-  props: PropTypes.string,
+  path: Proptypes.string,
+  props: Proptypes.string,
 }.isRequired;
+
 export default InProgressFoods;
