ad05e2626866503cd05b2058d06a6fdd5a154ba8
Telme <telmaeugenio@gmail.com>
2022-04-06 15:35:40 -0300

    start

--- src/pages/InProgressFoods.jsx
+++ src/pages/InProgressFoods.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InProgressFoods = (props) => {
+function InProgressFoods(props) {
   const { match: { params: { id } } } = props;
+  // Desenvolva um botão de nome "Start Recipe" que deve ficar fixo na parte de baixo da tela o tempo todo
+  const style = {
+    position: 'fixed',
+    bottom: '0',
+  };
   return (
     <div>
-      InProgressFoods - Start
+
+      <h1
+        data-testid="recipe-title"
+      >
+        Foods Details
+      </h1>
+      <img
+        src="recipePhoto"
+        alt="recipefoods"
+        data-testid="recipe-photo"
+      />
+      <section>
+        <h2>Category</h2>
+        <p data-testid="recipe-category">category</p>
+        <h2>Ingredients</h2>
+        <ul data-testid="0-ingredient-name-and-measure">
+          <li>Ingredients</li>
+        </ul>
+        <h2>Instructions</h2>
+        <p data-testid="instructions">instructions</p>
+      </section>
+      <button type="button" data-testid="share-btn">Share</button>
+      <label htmlFor="favorite-btn">
+        <input type="checkbox" data-testid="favorite-btn" id="favorite-btn" />
+        Favorite
+      </label>
+      <button
+        style={ style }
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Finish Recipe
+      </button>
       { id }
     </div>
 
   );
-};
+}
 
 InProgressFoods.propTypes = {
   id: PropTypes.string,
